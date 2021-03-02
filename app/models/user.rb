bbef705a6f076d18aee40a9de9c3124f418afec2
taiki yamashita <taiki0808ty@gmail.com>
2021-03-02 21:19:45 +0900

    profiles_controllerを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :profile, dependet: :destroy
+  has_one :profile, dependent: :destroy
 end
