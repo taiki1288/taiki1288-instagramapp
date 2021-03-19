b1b4049c9fce479efa2be59e0359d3e08483b1fc
taiki yamashita <taiki0808ty@gmail.com>
2021-03-19 18:28:17 +0900

    slickを導入して画像をスライドできるように実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+var jQuery = require('jquery')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +19,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(function() {
+    $('.slick').slick({
+        dots: true,
+        // autoplay: true,
+        // autoplaySpeed: 1000,
+    });
+});
