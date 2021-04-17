b47443d1c8c0920cd35591d97df9d1abf3f10250
taiki yamashita <taiki0808ty@gmail.com>
2021-04-17 18:56:53 +0900

    コメント機能エラー解決

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,24 +40,6 @@ $(function() {
 //   }
 // }
 
-const appendNewComment = (comment) => {
-  $('.comments-container').append(
-    `<div class='comment-card'>
-      <div class='comment-user-image'>
-        <img src="${comment.user.comment_avatar_image}">
-      </div>
-      <div class="user-status">
-        <div class="comment-user-name">
-          <p>${comment.user.account}</p>
-        </div>
-        <div class="timeline_comment">
-          <p>${comment.content}</p>
-        </div>
-      </div>
-     </div>`      
-  )
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     $('.timeline-card').each(function(){
       console.log($(this).data('timeline-id'));
@@ -76,31 +58,6 @@ document.addEventListener('DOMContentLoaded', () => {
           handleHeartDisplay(hasLiked)
         })
     });
-
-    const timelineId = $('.comments-container').attr('id')
-    axios.get(`/api/timelines/${timelineId}/comments`)
-    .then((response) => {
-      const comments = response.data
-      comments.forEach((comment) => {
-        appendNewComment(comment)
-      })
-    })
-
-    $('.create-comment-btn').on('click', () => {
-      const content = $('#comment_content').val()
-      if (!content) {
-        window.alert('コメントを入力してください')
-      } else {
-        axios.post(`/api/timelines/${timelineId}/comments`, {
-          comment: {content: content}
-        })
-          .then((res) => {
-            const comment = res.data
-            appendNewComment(comment)
-            $('#comment_content').val('')
-          })
-      }
-    })
 
     $('.inactive-heart').on('click', (e) => {
       e.preventDefault();
