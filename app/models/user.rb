a1383d7c898378e6409257a956e1f5ce699388f1
taiki yamashita <taiki0808ty@gmail.com>
2021-04-20 11:07:52 +0900

    フォローを外すメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,11 @@ class User < ApplicationRecord
     following_relationships.create!(following_id: user.id)
   end
 
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
+  end
+
   def avatar_image
     if profile&.avatar&.attached?
       profile.avatar
