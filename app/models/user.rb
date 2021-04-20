52faee1c784b7394f06d48b6ca1fe72adcaf35b2
taiki yamashita <taiki0808ty@gmail.com>
2021-04-20 10:45:23 +0900

    フォロワーの関係を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,9 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   def has_liked?(timeline)
     likes.exists?(timeline_id: timeline.id)
   end
