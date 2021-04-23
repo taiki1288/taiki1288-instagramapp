e62b1be5b8697431a713e3b936073449cc7ef110
taiki yamashita <taiki0808ty@gmail.com>
2021-04-23 19:52:59 +0900

    フォロー一覧ページ、フォロワー一覧ページを実装

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,8 +3,8 @@ class AccountsController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        if @user == current_user
-            redirect_to profile_path
-        end
+        # if @user == current_user
+        #     redirect_to profile_path
+        # end
     end
 end
