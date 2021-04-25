ab1242377b3c21f61f1576c68955e30a9e94e31f
taiki yamashita <taiki0808ty@gmail.com>
2021-04-25 15:23:05 +0900

    no message

--- app/controllers/unfollows_controller.rb
+++ app/controllers/unfollows_controller.rb
@@ -4,16 +4,16 @@ class UnfollowsController < ApplicationController
     def show
         user = User.find(params[:account_id])
         follow_status = current_user.has_followed?(user)
-        follower_count = user.followers.count
-        following_count = user.followings.count
+        follower_count = user.follower_counts(user)
+        following_count = user.following_counts(user)
         render json: { hasFollowed: follow_status, followerCounts: follower_count, followingCounts: following_count }
     end
 
     def create
         user = User.find(params[:account_id])
         current_user.unfollow!(user)
-        follower_count = user.followers.count
-        following_count = user.followings.count
+        follower_count = user.follower_counts(user)
+        following_count = user.following_counts(user)
         render json: { status: 'ok', followerCounts: follower_count, followingCounts: following_count }
     end
 end
